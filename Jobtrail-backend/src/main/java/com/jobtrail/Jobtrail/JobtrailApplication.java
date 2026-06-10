55ddc36dc2db83f4d84b200bf3412a9d644c116c
Naftal <naftalbosire1206@gmail.com>
2026-06-10 14:18:19 +0300

    initial commit

---
+++ Jobtrail-backend/src/main/java/com/jobtrail/Jobtrail/JobtrailApplication.java
@@ -0,0 +1,13 @@
+package com.jobtrail.Jobtrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobtrailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobtrailApplication.class, args);
+	}
+
+}
---
+++ Jobtrail-backend/src/test/java/com/jobtrail/Jobtrail/JobtrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobtrail.Jobtrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobtrailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
